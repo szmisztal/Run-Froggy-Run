20877cdfb01845e4708d6438709acdba89ac6672
szmisztal <sz.misztal@gmail.com>
2025-06-24 14:33:05 +0200

    add signals, add die(), fix player start position on middle of screen

--- player.gd
+++ player.gd
@@ -1,11 +1,16 @@
 extends Area2D
+signal pickup
+signal hurt
+
 
 @export var speed = 350
 var velocity = Vector2.ZERO
 var screensize = Vector2(480, 720)
 
-func _ready():
-	pass
+func start():
+	set_process(true)
+	position = screensize / 2
+	$AnimatedSprite2D.animation = "idle"
 	
 func _process(delta):
 	velocity = Vector2(0, 0)
@@ -21,3 +26,24 @@ func _process(delta):
 	position += velocity * speed * delta
 	position.x = clamp(position.x, 0, screensize.x)
 	position.y = clamp(position.y, 0, screensize.y)
+
+
+	if velocity.length() > 0:
+		$AnimatedSprite2D.animation = "run"
+	else:
+		$AnimatedSprite2D.animation = "idle"
+		
+	if velocity.x != 0:
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+		
+func die():
+	$AnimatedSprite2D.animation = "hurt"
+	set_process(false) 
+
+func _on_area_entered(area):
+	if area.is_in_group("fruits"):
+		area.pickup()
+		pickup.emit()
+	if area.is_in_group("obstacles"):
+		area.hurt()
+		die()
